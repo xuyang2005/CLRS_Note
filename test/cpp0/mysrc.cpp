eb8ee151f65b744a305752263289eaa617023dda
regulus <206465633@qq.com>
2024-11-26 01:56:50 +0800

    Bs

--- test/cpp0/mysrc.cpp
+++ test/cpp0/mysrc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int INF = 0x7f7f7f7f;
 
 int main() {int T;cin >> T;for (int i = 0; i < T; i++) {
-    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     //主函数
     
 }return 0;}
--- test/cppC/mysrc.cpp
+++ test/cppC/mysrc.cpp
@@ -1,8 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef long long lint;
 const int INF = 0x7f7f7f7f;
 
-int main() {int T;cin >> T;for (int i = 0; i < T; i++) {
-    //主函数
-}return 0;}
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;
+    vector<lint> bp;
+    vector<bool> b;
+    cin >> n;
+    for(int i = 0; i <= n; i++){
+        bp.push_back(0);
+        b.push_back(false);
+    }
+    int m;
+    cin >> m;
+    int l,r;
+    cin >> l >> r;
+    for(int i = 0; i < m; i++){
+        int temp;
+        cin >> temp;
+        b[temp] = true;
+    }
+
+    bp[0] = 1;
+    b[0] = true;
+    b[n] = true;
+    for(int i = 0; i < n; i++){
+        int il = i + l;
+        int ir = i + r;
+        if(ir>n){
+            ir = n;
+        }
+        for(int j = il; j <= ir; j++){
+            if(b[j]){
+                bp[j] += bp[i];
+                if(bp[j] > 998244353){
+                bp[j] %= 998244353;
+                }
+                //cout << "bp[" << j << "]=" << bp[j] << endl; 
+            }
+        }
+    }
+    int result = bp[n] % 998244353;
+    cout << result << endl;
+    return 0;}
