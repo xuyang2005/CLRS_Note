541a602274a312f5271dfaa271bd27fab1ebc0be
regulus <206465633@qq.com>
2024-11-27 16:44:27 +0800

    Fwa2

--- test/cpp0/mysrc.cpp
+++ test/cpp0/mysrc.cpp
@@ -6,7 +6,7 @@ typedef long long lint;
 
 int main() {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int T;cin >> T;for (int i = 0; i < T; i++) {
+    int T;cin >> T;while(T--) {
     //主函数
     }
 return 0;}
--- test/cppF/mysrc.cpp
+++ test/cppF/mysrc.cpp
@@ -1,117 +1,114 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int INF = 0x7f7f7f7f;
-typedef long long lint;
-typedef pair<long long,long long> point;
-
+typedef pair<__int128,__int128> point;
+typedef __int128 lint;
 void f(){
-    //主函数
     vector<point> ss;
-    lint n;
-    cin >> n;
-    lint x, y;
-    cin >> x >> y;
+    long long n1;
+    cin >> n1;
+    lint n = n1;
+    long long x1, y1;
+    cin >> x1 >> y1;
+    lint x = x1;
+    lint y = y1;
     point findP{x,y};
     point zeroP{0,0};
     ss.push_back(zeroP);
     for(lint i = 0; i < n; i++){
         char temp;
         cin >> temp;
-        if(temp=='U'){
+        if(temp == 'U') {
             zeroP.second += 1;
+            
         ss.push_back(zeroP);
-        }else if(temp=='D'){
+        } else if(temp == 'D') {
             zeroP.second -= 1;
+            
         ss.push_back(zeroP);
-        }else if(temp=='L'){
+        } else if(temp == 'L') {
             zeroP.first -= 1;
+            
         ss.push_back(zeroP);
-        }else if(temp=='R'){
+        } else if(temp == 'R') {
             zeroP.first += 1;
+            
         ss.push_back(zeroP);
-        }else{
+        } else {
             i--;
         }
     }
-    lint rx = ss[n].first;
-    lint ry = ss[n].second;
-    // for(auto& ele : ss){
-    //     cout << ele.first << "," << ele.second << endl;
-    // }
+
     lint result = -1;
     lint h = -1;
     lint hc = -1;
-    for(lint i = 0; i < n; i++){
-        // cout << "rx=" << rx << " " << "ry=" << ry << endl;
-        // cout << "ss[i].first=" << ss[i].first << endl;
-        // cout << "ss[i].second=" << ss[i].second << endl;
-        // cout << "x=" << x << " " << "y=" << y << endl;
-        if(rx==0&&ry==0){
-            if(ss[i]==findP){
+
+    point finalPos = ss[n];
+
+    for(long long i = 0; i < n; i++){
+        long long rx = finalPos.first;
+        long long ry = finalPos.second;
+
+        if(rx == 0 && ry == 0){
+            if(ss[i] == findP){
                 h = 0;
                 hc = i;
                 break;
             }
-        }else if(rx==0){
-            if(x==ss[i].first && (y - ss[i].second)%ry==0){
-                lint hg = abs((y - ss[i].second)/ry);
-                lint hcg = i;
-                if(h==-1){
+        } else if(rx == 0){
+            if(x == ss[i].first && (y - ss[i].second) % ry == 0
+            && ((y - ss[i].second) / ry)>0){
+                lint hg = (y - ss[i].second) / ry;
+                if(hg<0){
+                    hg *= -1;
+                }
+                if(h == -1 || hg * n + i < h * n + hc){
                     h = hg;
-                    hc = hcg;
-                }else if((hg*n+hcg)<(h*n+hc)){
-                    h = hg;
-                    hc = hcg;
+                    hc = i;
                 }
             }
-        }else if(ry==0){
-            if(y==ss[i].second && (x - ss[i].first)%rx==0){
-                lint hg = abs((x - ss[i].first)/rx);
-                lint hcg = i;
-                if(h==-1){
+        } else if(ry == 0){
+            if(y == ss[i].second && (x - ss[i].first) % rx == 0
+            && ((x - ss[i].first) / rx)>0){
+                lint hg = (x - ss[i].first) / rx;
+                if(hg<0){
+                    hg *= -1;
+                }
+                if(h == -1 || hg * n + i < h * n + hc){
                     h = hg;
-                    hc = hcg;
-                }else if((hg*n+hcg)<(h*n+hc)){
-                    h = hg;
-                    hc = hcg;
+                    hc = i;
                 }
             }
-        }else{
-            if((y - ss[i].second)%ry==0){
-                if((x - ss[i].first)%rx==0){
-                    lint h1 = (y - ss[i].second)/ry;
-                    lint h2 = (x - ss[i].first)/rx;
-                    // cout << "h1=" << h1 << endl;
-                    // cout << "h2=" << h2 << endl;
-                    if(h1==h2){
-                        lint hg = abs((x - ss[i].first)/rx);
-                        // cout << "hg=" << hg << endl;
-                        lint hcg = i;
-                        // cout << "hcg=" << hcg << endl;
-                        if(h==-1){
-                            h = hg;
-                            hc = hcg;
-                        }else if((hg*n+hcg)<(h*n+hc)){
-                            h = hg;
-                            hc = hcg;
-                        }
+        } else{
+            if((y - ss[i].second) % ry == 0 && (x - ss[i].first) % rx == 0){
+                lint h1 = (y - ss[i].second) / ry;
+                lint h2 = (x - ss[i].first) / rx;
+                if(h1 == h2){
+                    lint hg = h1;
+                    if(hg<0){
+                        hg *= -1;
+                    }
+                    if(h == -1 || hg * n + i < h * n + hc){
+                        h = hg;
+                        hc = i;
                     }
                 }
             }
         }
     }
-    if(h!=-1){
-        lint result = h * n + hc;
+
+    if(h != -1){
+        long long result = h * n + hc;
         cout << result << endl;
-    }else{
+    } else {
         cout << "-1" << endl;
     }
 }
 
 int main() {
-    //ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int T;cin >> T;for (int ii = 0; ii < T; ii++) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T; cin >> T; 
+    while (T--) {
         f();
     }
-return 0;}
+    return 0;
+}
